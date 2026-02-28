d3456549587852b784686a34fdfc63121c8fcf9e
pranitha <chikkelapranitha@gmail.com>
2026-02-28 22:31:17 +0530

    Initial frontend setup

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,8 @@
 import API from "./api";
 
 export const registerUser = (userData) => {
-  return API.post("/api/auth/register", userData);
+  return API.post("/auth/register", userData);
 };
 export const loginUser = (userData) => {
-  return API.post("/api/auth/login", userData);
+  return API.post("/auth/login", userData);
 };
